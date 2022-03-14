b2b34c8706a30805d07f818acf0749d773013805
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-03-14 11:22:28 +1000

    Completed Cook Dish lesson.

--- Education/arrays.rb
+++ Education/arrays.rb
@@ -3,7 +3,8 @@ shopping_list = []
 shopping_list.push('sugar', 'milk', 'coffee', 'bananas', 'chocolate')
 updated_shopping_list = shopping_list.clone
 updated_shopping_list.delete_at(1)
+updated_shopping_list.insert(3, 'bread')
 
 #  puts shopping_list
-p shopping_list
+
 puts updated_shopping_list
--- Education/modulesandclasses.rb
+++ Education/modulesandclasses.rb
@@ -1,17 +1,50 @@
 # Lesson 1 Week 5
 
-puts 'stir coffee'
 # Method to add sugar
-def add_sugar(numteaspoons)
-  numteaspoons.times do
+def add_sugar(num_teaspoons)
+  num_teaspoons.times do
     puts 'Get a teaspoon of sugar'
     puts 'Drop sugar into coffee'
   end
 end
 
-# Method to stir coffee
+#  Method to stir coffee
 def stir
   puts 'Stir Coffee'
 end
 
-add_sugar(3)
+# Method that returns a user input for numb of sugars.
+def get_sugars
+  puts 'How many sugars?'
+  gets.chomp.to_i
+end
+
+def boil_water
+  puts 'Fill Saucepan with water'
+  puts 'Place saucepan on stove'
+  puts 'Bring water to boil'
+end
+
+def print_heading(dish_name)
+  puts '_' * 20
+  puts "How to cook #{dish_name}"
+  puts '_' * 20
+end
+
+def cook_pasta
+  print_heading('pasta')
+  boil_water
+  puts 'Add salt'
+  puts 'Add pasta'
+  puts 'Cook for 5 mins'
+end
+
+def cook_rice
+  print_heading('rice')
+  boil_water
+  puts 'Add rice'
+  puts 'Cook for 10 mins'
+end
+
+cook_pasta
+cook_rice
