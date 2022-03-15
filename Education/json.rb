16688e826636816009698e15eb86363da95fe66d
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-03-15 14:39:54 +1000

    Lesson 2 Week 5 Completed

--- Education/json.rb
+++ Education/json.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
+
 require 'json'
 
 # data = File.read('books.json')
 parsed = JSON.load_file('json.json', symbolize_names: true)
-puts parsed
+parsed[0][:books] << "Buy Jupiter"
+File.write('json.json', JSON.pretty_generate(parsed))
